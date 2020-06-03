2899e8f3f37dbbe68d7fb9f4cd44456c182ee3f4
Just-a-Programmer <manan18294@iiitd.ac.in>
2020-06-03 12:03:59 +0530

    Added install script

--- cp.sh
+++ cp.sh
@@ -4,7 +4,7 @@ red="\033[1;31;40m"
 blue="\033[1;36;40m"
 purple="\033[1;35;40m"
 green="\033[1;32;40m"
-mypath=~/CP_Parser/
+mypath=~/.cp
 function cpfetch()
 {
     dir=$(pwd)
@@ -17,34 +17,38 @@ function cprun()
 {
     if [ ! -d "Testcase" ]
     then
-       echo $red"Fetch Testcases First"
+       echo $red"Fetch Testcases First"$nc
        return
     fi
     if [ $# -eq 0 ]
     then
-        echo $red"Please Provide a file"
+        echo $red"Please Provide a file"$nc
         return
     fi
+    #Change to the extension of your language
     pre=${1%.cpp}
     dir=$(pwd)
     file=$dir/$1
+    #Change to the extension of your language
     filename=${file%.cpp}
+    #Use the compilation command
     g++-9 --std=c++17 -Wall -O2 $file -o $filename
     c=$(find Testcase/ -name "$pre*.in" | wc -l)
     for ((i = 1 ; i <= $c ; i++))
     do
+        #Subsitute running of your language, don't change redirection 
         $filename < "./Testcase/$pre$i.in" > myoutput.out
-        echo $yellow"\nYour Output:"$green
+        echo $yellow"\nYour Output:"$nc
         cat myoutput.out
-        echo $yellow"\nExpexted Output:"$green
+        echo $yellow"\nExpexted Output:"$nc
         cat "./Testcase/$pre$i.out"
         DIFF=$(diff "./Testcase/$pre$i.out" myoutput.out)
         if [ "$DIFF" != "" ]
         then
-            echo $red"Failed"
+            echo $red"Failed"$nc
             echo $DIFF
         else
-            echo $blue"\nAccepted"
+            echo $blue"\nAccepted"$nc
         fi
     done
 }
---
+++ install.sh
@@ -0,0 +1,3 @@
+mkdir ~/.cp
+cp Parser.py cp.sh ~/.cp
+echo "source ~/.cp/cp.sh" >> ~/.bashrc
